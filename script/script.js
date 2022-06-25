d7c6cf951d787378e73f782a88a212a83509f7e1
Rejwan Islam <rejwanislamrizvy7@gmail.com>
2022-06-25 22:59:32 +0600

    factoring

--- script/script.js
+++ script/script.js
@@ -12,16 +12,19 @@ window.onload = (e) => {
         // stop form from refreshing the page
         e.preventDefault();
         
-        var course1 = parseFloat(form['course1'].value*PER_COURSE);
-        var course2 = parseFloat(form['course2'].value*PER_COURSE);
-        var course3 = parseFloat(form['course3'].value*PER_COURSE);
-        var course4 = parseFloat(form['course4'].value*PER_COURSE);
-        var course5 = parseFloat(form['course5'].value*PER_COURSE);
-        var lab1 = parseFloat(form['lab1'].value*PER_LAB);
-        var lab2 = parseFloat(form['lab2'].value*PER_LAB);
-        var result =((course1)+(course2)+(course3)+(course4)+(course5)+(lab1)+(lab2))/(CREDIT);
+        var course1 = parseFloat(form['course1'].value * PER_COURSE);
+        var course2 = parseFloat(form['course2'].value * PER_COURSE);
+        var course3 = parseFloat(form['course3'].value * PER_COURSE);
+        var course4 = parseFloat(form['course4'].value * PER_COURSE);
+        var course5 = parseFloat(form['course5'].value * PER_COURSE);
+
+        var lab1 = parseFloat(form['lab1'].value * PER_LAB);
+        var lab2 = parseFloat(form['lab2'].value * PER_LAB);
+
+        var result =(course1 + course2 + course3 + course4 + course5 + lab1 + lab2) /(CREDIT);
         result= result.toFixed(2);
 
+        // showing output
         document.querySelector("#final-gpa").innerHTML = result;
     });
 
