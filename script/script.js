85490efad31739a52aca90de0aaf779b176e4b91
Rejwan Islam <rejwanislamrizvy7@gmail.com>
2022-06-28 23:36:51 +0600

    readable variable name

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 var CREDIT = 18;
-var PER_COURSE = 3;
-var PER_LAB = 1.5;
+var CREDIT_PER_COURSE = 3;
+var CREDIT_PER_LAB = 1.5;
 
 
 window.onload = (e) => {
@@ -12,14 +12,14 @@ window.onload = (e) => {
         // stop form from refreshing the page
         e.preventDefault();
         
-        var course1 = parseFloat(form['course1'].value * PER_COURSE);
-        var course2 = parseFloat(form['course2'].value * PER_COURSE);
-        var course3 = parseFloat(form['course3'].value * PER_COURSE);
-        var course4 = parseFloat(form['course4'].value * PER_COURSE);
-        var course5 = parseFloat(form['course5'].value * PER_COURSE);
+        var course1 = parseFloat(form['course1'].value * CREDIT_PER_COURSE);
+        var course2 = parseFloat(form['course2'].value * CREDIT_PER_COURSE);
+        var course3 = parseFloat(form['course3'].value * CREDIT_PER_COURSE);
+        var course4 = parseFloat(form['course4'].value * CREDIT_PER_COURSE);
+        var course5 = parseFloat(form['course5'].value * CREDIT_PER_COURSE);
 
-        var lab1 = parseFloat(form['lab1'].value * PER_LAB);
-        var lab2 = parseFloat(form['lab2'].value * PER_LAB);
+        var lab1 = parseFloat(form['lab1'].value * CREDIT_PER_LAB);
+        var lab2 = parseFloat(form['lab2'].value * CREDIT_PER_LAB);
 
         var result =(course1 + course2 + course3 + course4 + course5 + lab1 + lab2) /(CREDIT);
         result= result.toFixed(2);
