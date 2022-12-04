211257a09c26c21970d0de7501de6c06f79b96ba
Rejwan Islam Rizvy <50569315+RIR360@users.noreply.github.com>
2022-12-04 12:30:40 +0600

    add input functionality

--- script/script.js
+++ script/script.js
@@ -2,6 +2,55 @@ var CREDIT = 18;
 var CREDIT_PER_COURSE = 3;
 var CREDIT_PER_LAB = 1.5;
 
+function add_input(type) {
+    if (type == "course") {
+        let course_con = document.getElementById("course-container");
+        let inputs_available = course_con.querySelectorAll(".input-group").length;
+        let input_group = document.createElement("div");
+        input_group.classList = "input-group py-1";
+        input_group.innerHTML = `
+            <input type="text" class="form-control" placeholder="Course Name">
+            <select name="course${inputs_available+1}" class="form-control" required>
+                <option value="" selected>Choose Your Grade</option>
+                <option value="4.00">A+</option>
+                <option value="3.75">A</option>
+                <option value="3.50">A-</option>
+                <option value="3.25">B+</option>
+                <option value="3.00">B</option>
+                <option value="2.75">B-</option>
+                <option value="2.50">C+</option>
+                <option value="2.25">C</option>
+                <option value="2.00">D</option>
+            </select>
+        `;
+        course_con.appendChild(input_group);
+
+    } else if (type == "lab") {
+        let lab_con = document.getElementById("lab-container");
+        let inputs_available = lab_con.querySelectorAll(".input-group").length;
+        let input_group = document.createElement("div");
+        input_group.classList = "input-group py-1";
+        input_group.innerHTML = `
+            <input type="text" class="form-control" placeholder="Lab Name">
+            <select name="lab${inputs_available+1}" class="form-control" required>
+                <option value="" selected>Choose Your Grade</option>
+                <option value="4.00">A+</option>
+                <option value="3.75">A</option>
+                <option value="3.50">A-</option>
+                <option value="3.25">B+</option>
+                <option value="3.00">B</option>
+                <option value="2.75">B-</option>
+                <option value="2.50">C+</option>
+                <option value="2.25">C</option>
+                <option value="2.00">D</option>
+            </select>
+        `;
+        lab_con.appendChild(input_group);
+    } else {
+        alert("Type not given");
+    }
+}
+
 
 window.onload = (e) => {
 
